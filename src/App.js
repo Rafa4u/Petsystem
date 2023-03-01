ac56b47a6f826fd3454786d5507e9ea4ace4870c
Rafa4u <fontana.rafael@gmail.com>
2023-03-01 17:27:50 -0300

    with egnerator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
 import ButtonAppBar from './NavBar';
-import Recipe from './Card';
 import Simple from './List';
-import qrcode from './QR_code.png';
+// import qrcode from './QR_code.png';
 import Logo from './logo.png';
+import Qrcodefunction from './Qrcode';
+import Generator from './QrCodeGenerator';
 
 
 
@@ -13,8 +14,8 @@ function App() {
     <div className="App">
     <ButtonAppBar />  
     <img src={Logo} alt="company logo" /><br></br>
-    <img src={qrcode} alt="qrcode" width="300" height="300" />
-    <Recipe />
+    <Qrcodefunction />
+    <Generator />
     <Simple />
     </div>
     
---
+++ src/Qrcode.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Qrcodefunction() {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+    </div>
+  );
+}
